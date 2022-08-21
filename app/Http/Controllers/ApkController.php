94999f550f7dd028646dbad1e3f8e0ca1400acbc
Mai Viết Dũng <no_body156@yahoo.com.vn>
2022-08-22 04:50:05 +0700

    Edit Version Detect

--- app/Http/Controllers/ApkController.php
+++ app/Http/Controllers/ApkController.php
@@ -198,9 +198,9 @@ class ApkController extends Controller
         if (array_key_exists("packageName", $app) && ($app["price"] == "0" || $app["price"] == null )){ // app found and free
             // update version info
             if($app["versionName"] == "Varies with device"){
-                $crawler = GoutteFacade::request('GET', 'https://apksos.com/app/'. $id);
-                $filterData = $crawler->filter('div.section.row > div.col-sm-12.col-md-8 > ul > li:nth-child(1)')->text();
-                $versionText = str_replace("Version: ", "", $filterData);
+//                $crawler = GoutteFacade::request('GET', 'https://apksos.com/app/'. $id);
+//                $filterData = $crawler->filter('div.section.row > div.col-sm-12.col-md-8 > ul > li:nth-child(1)')->text();
+//                $versionText = str_replace("Version: ", "", $filterData);
                 $app["versionName"] = $appRequest->version;
             }
 //            $directLink = $this->getApiStore($id); // If you want to revert to apistore api, uncomment this
